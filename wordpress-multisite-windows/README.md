b690f98e0a57c68265a00a40c79e035e30975f0a
tyler.evert <tyler.evert@centare.com>
2016-02-17 14:07:11 -0600

    Adding wordpress bootstrap with inital site configuration.

---
+++ wordpress-multisite-windows/README.md
@@ -0,0 +1,10 @@
+# Deploy a single-VM WordPress to Azure, with multi-site configured
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Ftevert%2Fazure-quickstart-templates%2Fmaster%2Fwordpress-multisite-ubuntu%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Ftevert%2Fazure-quickstart-templates%2Fmaster%2Fwordpress-multisite-ubuntu%2Fazuredeploy.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
+
+This template deploys a complete LAMP stack, then installs WordPress and configures it for a multi-site setup.
---
+++ wordpress-multisite-windows/install_wordpress.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+apt-get -y update
+
+logger "Installing WordPress"
+
+# Set up a silent install of MySQL
+dbpass=$3
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+# Install the LAMP stack and WordPress
+apt-get -y install apache2 mysql-server php5 php5-mysql wordpress
+
+# Setup WordPress
+gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
+bash /usr/share/doc/wordpress/examples/setup-mysql -n wordpress localhost
+
+ln -s /usr/share/wordpress /var/www/html/wordpress
+mv /etc/wordpress/config-localhost.php /etc/wordpress/config-default.php
+
+# Restart Apache
+apachectl restart
+
+logger "Done installing WordPress; beginning configuration"
+
+# Grab the WordPress CLI utility and install it (http://wp-cli.org/)
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp
+chmod +x /usr/local/bin/wp
+
+# Create the base site
+wp core install --url="http://localhost/wordpress/" --title="Main Wordpress Site" --admin_user="$1" --admin_password="$2" --admin_email="$1@changeme.please"
+
+# Enable multi-site
+
+logger "Done configuring"
