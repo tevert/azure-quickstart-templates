85c57e6d5139ad7332846d44b7eec725b055b3e1
tyler.evert <tyler.evert@centare.com>
2016-02-26 11:01:40 -0600

    We don't need to be using sudo; this already runs as root

--- wordpress-multisite-ubuntu/install_wordpress.sh
+++ wordpress-multisite-ubuntu/install_wordpress.sh
@@ -11,7 +11,7 @@ export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
 echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
-# Install the LAMP stack and WordPress
+# Install the LAMP stack
 apt-get -y install apache2 mysql-server php5 php5-mysql wordpress
 
 # Setup WordPress
@@ -27,14 +27,14 @@ apachectl restart
 echo "Done installing WordPress; beginning configuration"
 
 # Grab the WordPress CLI utility and install it (http://wp-cli.org/)
-sudo curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp
-sudo chmod +x /usr/local/bin/wp
+curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp
+chmod +x /usr/local/bin/wp
 
 # Create the base site
 serverRegion=$(echo "$5" | tr -d '[[:space:]]' | tr '[:upper:]' '[:lower:]')
 serverUrl="http://$4.$serverRegion.cloudapp.azure.com/wordpress/"
 siteName="Main Wordpress Site"
 echo "Creating wordpress site at $serverUrl named $siteName with admin username $1"
-sudo wp core multisite-install --url="$serverUrl" --title="$siteName" --admin_user="$1" --admin_password="$2" --admin_email="$1@example.com" --skip-email --path=/var/www/html/wordpress --base=/wordpress --allow-root
+wp core multisite-install --url="$serverUrl" --title="$siteName" --admin_user="$1" --admin_password="$2" --admin_email="$1@example.com" --skip-email --path=/var/www/html/wordpress --base=/wordpress --allow-root
 
 echo "Done configuring"
